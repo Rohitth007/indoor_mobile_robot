a27600c21289449c82a490c7562f4a1aa889b765
BijoSebastian <bijosebastian757@gmail.com>
2022-07-26 18:01:44 +0530

    Added arduino files

---
+++ Arduino/motor_control_urc10/config.h
@@ -0,0 +1,22 @@
+//Pins for Motor 1
+#define RH_ENCODER_A 3 
+#define RH_ENCODER_B 9
+#define RH_PWM 6
+#define RH_DIR 7
+
+//Pins for Motor 2
+#define LH_ENCODER_A 2
+#define LH_ENCODER_B 8
+#define LH_PWM 5
+#define LH_DIR 4
+
+#define GR 50.9 // Gear Ratio
+#define PPR 7.0 // Pulse Per Revolution
+
+#define MIN_PWM 25
+#define MIN_SPD 10.12 // in revolutions per second
+#define MAX_SPD 115 // in revolutions per second
+
+// Proportional speed controller
+#define left_motor_gain 1.1  
+#define right_motor_gain 1.1 
---
+++ Arduino/motor_control_urc10/motor_control_urc10.ino
@@ -0,0 +1,266 @@
+#include "config.h"
+
+//For serial communication
+char receivedChars[12];
+boolean newData = false;
+
+//For time measurment 
+unsigned long currTime=0, prevTime=0; // time measurment
+double dt = 0.0; //deltatime in seconds 
+
+//keeping it safe
+long encoder_minimum = -2147483640; 
+long encoder_maximum =  2147483640;
+
+//'Motor' Class for specific data corresponding to each motor
+class Motor
+{   
+  
+  public:
+    float desiredSpeed = 0; 
+    float currentSpeed = 0;
+    long encoderCount = 0;
+    long prevEncoderCount = 0;
+    int output;
+    float gain = 0;
+    
+    void setDesiredSpeed(float s)
+    // s is desired speed in revolutions per second
+    {
+       if(abs(s)>MAX_SPD)
+       {
+        desiredSpeed = MAX_SPD;
+        return;
+       }
+       
+       if(abs(s)<MIN_SPD)
+       {
+        desiredSpeed = 0;
+        return;
+       }
+      desiredSpeed = s;
+    }
+    
+    float computeOutput(float dt)
+    //dt is deltatime in seconds
+    {
+        currentSpeed = ((encoderCount-prevEncoderCount)/(PPR*dt*GR));
+        prevEncoderCount = encoderCount;
+        output = gain*(desiredSpeed-currentSpeed);
+    }
+    
+    Motor(float motorGain)
+    //Constructor for Motor Class
+    {
+      gain = motorGain;  
+    }
+    
+};
+
+Motor leftMotor(left_motor_gain);
+Motor rightMotor(right_motor_gain);
+
+void setup() 
+{
+
+  leftMotor.setDesiredSpeed(0.0);
+  rightMotor.setDesiredSpeed(0.0);
+
+  pinMode(LH_ENCODER_A, INPUT);
+  pinMode(LH_ENCODER_B, INPUT);
+  pinMode(RH_ENCODER_A, INPUT);
+  pinMode(RH_ENCODER_B, INPUT);
+  
+  pinMode(RH_DIR,OUTPUT);
+  pinMode(LH_DIR,OUTPUT);
+  pinMode(RH_PWM,OUTPUT);
+  pinMode(LH_PWM,OUTPUT);
+
+  // initialize hardware interrupts for RISE event
+  attachInterrupt(digitalPinToInterrupt(LH_ENCODER_A), leftEncoderRiseEvent, RISING);
+  attachInterrupt(digitalPinToInterrupt(RH_ENCODER_A), rightEncoderRiseEvent, RISING);
+  
+  // initialize hardware interrupts for FALL event
+  attachInterrupt(digitalPinToInterrupt(LH_ENCODER_A), leftEncoderRiseEvent, FALLING);
+  attachInterrupt(digitalPinToInterrupt(RH_ENCODER_A), rightEncoderRiseEvent, FALLING);
+
+  Serial.begin(9600);
+  //Data expected in format<left_speed, right _speed>
+
+  Serial.println("<Arduino is ready>");
+}
+
+void loop() 
+{
+  //Get commanded speed
+  recvWithStartEndMarkers();
+  if (newData == true)
+  {
+    parseData();
+    newData = false;  
+  }
+
+  //Compute delta time
+  currTime = micros(); 
+  dt =(currTime - prevTime)/(60.0*(1000000.0));   
+  prevTime = currTime;
+
+  //Run Proportional speed controller and get output
+  rightMotor.computeOutput(dt);
+  leftMotor.computeOutput(dt);
+
+  //Apply control signal on the motors
+  if(rightMotor.output>0) 
+  {
+    digitalWrite(RH_DIR, 0);
+  }
+  else 
+  {
+    digitalWrite(RH_DIR, 1);
+  }
+  if(leftMotor.output>0) 
+  {
+    digitalWrite(LH_DIR, 0);
+  }
+  else 
+  {
+    digitalWrite(LH_DIR, 1);
+  }
+ 
+  if(abs(rightMotor.output)>255)
+  {
+    rightMotor.output = 255;       
+  }
+  if(abs(rightMotor.output)<MIN_PWM)
+  {
+    rightMotor.output = 0;       
+  }
+  if(abs(leftMotor.output)>255)
+  {
+    leftMotor.output = 255;       
+  }
+  if(abs(leftMotor.output)<MIN_PWM)
+  {
+    leftMotor.output = 0;       
+  }
+  analogWrite(LH_PWM, abs(leftMotor.output));
+  analogWrite(RH_PWM, abs(rightMotor.output));  
+
+  //return current tick count
+  Serial.print("<");
+  Serial.print(round(leftMotor.encoderCount));
+  Serial.print(",");
+  Serial.print(round(rightMotor.encoderCount));
+  Serial.print(">\n");
+}
+
+//============
+void leftEncoderRiseEvent() 
+{
+  if (digitalRead(LH_ENCODER_B) == HIGH) 
+  {
+    if (leftMotor.encoderCount < encoder_maximum) 
+      leftMotor.encoderCount++; 
+  }
+  else 
+  {
+    if (leftMotor.encoderCount > encoder_minimum) 
+      leftMotor.encoderCount--;
+  }
+}
+
+//============
+void leftEncoderFallEvent() 
+{
+  if (digitalRead(LH_ENCODER_B) == LOW) 
+  {
+    if (leftMotor.encoderCount < encoder_maximum) 
+      leftMotor.encoderCount++;
+  }
+  else 
+  {
+    if (leftMotor.encoderCount > encoder_minimum) 
+      leftMotor.encoderCount--;
+  }
+}
+
+//============
+void rightEncoderRiseEvent() 
+{
+  if (digitalRead(RH_ENCODER_B) == HIGH)
+  {
+    if (rightMotor.encoderCount < encoder_maximum) 
+      rightMotor.encoderCount++;  
+  }
+  else 
+  {
+    if (rightMotor.encoderCount > encoder_minimum) 
+      rightMotor.encoderCount--;
+  }
+}
+
+//============
+void rightEncoderFallEvent() 
+{
+  if (digitalRead(RH_ENCODER_B) == LOW) 
+  {
+    if (rightMotor.encoderCount < encoder_maximum) 
+      rightMotor.encoderCount++;  
+  }
+  else 
+  {
+    if (rightMotor.encoderCount > encoder_minimum) 
+      rightMotor.encoderCount--;
+  }
+}
+
+//============
+void recvWithStartEndMarkers() 
+{
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0&& newData == false) 
+    {
+        rc = Serial.read();
+
+        if (recvInProgress == true) 
+        {
+            if (rc != endMarker)
+            {
+                receivedChars[ndx] = rc;
+                ndx++;
+            }
+            else
+            {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) 
+        {
+            recvInProgress = true;
+        }
+    }
+}
+
+//============
+
+void parseData()       
+// split the data into its parts
+{
+    char * strtokIndx; // this is used by strtok() as an index
+ 
+    strtokIndx = strtok(receivedChars, ","); // get the first part - left speed
+    leftMotor.setDesiredSpeed(atoi(strtokIndx));
+  
+    
+    strtokIndx = strtok(NULL, ","); // get the second part - right speed
+    rightMotor.setDesiredSpeed(atoi(strtokIndx)); 
+}
