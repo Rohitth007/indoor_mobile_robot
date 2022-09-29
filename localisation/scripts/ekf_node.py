ba8a854e832522aa5ccfa517ce1384a714d076ef
gokulbot <gokuljalaja@gmail.com>
2022-09-29 06:04:21 +0530

    added comments to the code

--- localisation/scripts/ekf_node.py
+++ localisation/scripts/ekf_node.py
@@ -9,7 +9,7 @@ import tf
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 from numpy import *
 
-#class to keep track of the ticks which arise from the motor encoders
+#class to keep track of the states and covariances
 class ExtendedKalmanFilter():
     state=[]
     covariance=[]
@@ -20,6 +20,7 @@ class ExtendedKalmanFilter():
     result=[]
 
     @staticmethod
+    #fucntion which relates between the stae and th reference walls 
     def h(state, landmark, scanner_displacement):
         dx = landmark[0] - (state[0] + scanner_displacement * cos(state[2]))
         dy = landmark[1] - (state[1] + scanner_displacement * sin(state[2]))
@@ -29,6 +30,7 @@ class ExtendedKalmanFilter():
         return array([r, alpha])
     
     @staticmethod
+    #function to return the covariance detected parameters of the wall 
     def get_covariance(p1,p2,m_radius,m_angle):
         pho_1 = p1[0]**2+p1[1]**2
         theta_1= atan2(p1[1],p1[0])
@@ -46,6 +48,7 @@ class ExtendedKalmanFilter():
 
 
     @staticmethod
+    #fucntion to calculate the jacobina of the measurement with respect to teh state 
     def dh_dstate(state, landmark, scanner_displacement):
 
         x, y, theta = state
@@ -71,12 +74,18 @@ class ExtendedKalmanFilter():
         return array([[dr_dx, dr_dy, dr_dtheta], [dalpha_dx, dalpha_dy, dalpha_dtheta]])
 
     def correction(self,map,r_ref_values,alpha_ref_values,r_values,alpha_values,endpoints_x,endpoints_y):
+        #loading the map
         wall_begin_points=map["wall_begin_points"]
         wall_end_points=map["wall_end_points"]
+
+        #converting to the world coordinate frame
         r_values,alpha_values,endpoints_x,endpoints_y= to_world(r_values,alpha_values,endpoints_x,endpoints_y)
+
+        #defining certain thresholds to be used for calculation
         best_dist = self.threshold
         best_r = None
 
+        #looping acroos the detected landmarks and refernce land marks to detec the best corellations 
         for i in range(len(r_values)):
             endpoint_x_begin = endpoints_x[2*i][0]
             endpoint_y_begin = wall_begin_points[2*i][1]
@@ -95,29 +104,34 @@ class ExtendedKalmanFilter():
                 endpoint_x_end_wall = wall_end_points[j][0]
                 endpoint_y_end_wall = wall_end_points[j][1]
 
+                #set of conditions to ensure that the detec walls lie within the refernce walls 
                 condition1 = endpoint_x_begin_wall<=endpoint_x_begin and endpoint_x_begin_wall<endpoint_x_end 
                 condition2 = endpoint_x_end_wall>endpoint_x_begin and endpoint_x_end_wall>=endpoint_x_end 
     
                 condition3 = endpoint_y_begin_wall<=endpoint_y_begin and endpoint_y_begin_wall<endpoint_y_end 
                 condition4 = endpoint_y_end_wall>endpoint_y_begin and endpoint_y_end_wall>=endpoint_y_end 
 
-
+                #detecting the best landmarks and adding them to  alist an returning it 
                 if condition1 and condition2 and condition3 and condition4:
                     if dist_2 < best_dist:
                         best_dist = dist_2
                         best_r,best_alpha = r_ref_values[j],alpha_ref_values[j]
                         endpoint_b = [endpoint_x_begin,endpoint_y_begin]
                         endpoint_e = [endpoint_x_end,endpoint_y_end]
-            # If found, add to both lists.
+       
             if best_r:
                 self.result.append(r_values[i],alpha_values[j],endpoint_b,endpoint_e,best_r,best_alpha)
         
+        #using the list created to perform coreection the predicted estimat of the robots pose
         for ele in result:
             landmark=(ele[0]*cos(ele[1]),ele[0]*sin(ele[1]))
+
+            #covarinace update and kalman gain calculation
             H_t = self.dh_dstate(self.state, landmark,self.scanner_displacement)
             Q = self.get_covariance(ele[2],ele[3],self.measurement_distance_stddev,self.measurement_angle_stddev)
             K_t = dot(dot(self.covariance, H_t.T), linalg.inv(dot(H_t, dot(self.covariance, H_t.T)) + Q))
 
+            #correction step and updation of the state in the ekf class
             innovation = array(ele[0],ele[1]) - self.h(self.state, landmark, self.scanner_displacement)
             innovation[1] = (innovation[1] + pi) % (2*pi) - pi
 
@@ -132,6 +146,7 @@ class Features:
     endpoints_y=[]
     number_lines=0
 
+#function for proper conversion from catesian to polar if the radius is negative
 def radius_negative_conversion(r,alpha):
 
     if r < 0:
@@ -141,6 +156,7 @@ def radius_negative_conversion(r,alpha):
         r = -r
     return r,alpha
 
+#function to convert the detected landmark positions to the world coordinate frame for comparison
 def to_world(r_values,alpha_values,endpoints_x,endpoints_y):
     global ekf
     r=[]
@@ -150,6 +166,7 @@ def to_world(r_values,alpha_values,endpoints_x,endpoints_y):
     dx = cos(ekf.state[2])
     dy = sin(ekf.state[2])
 
+    #converting the radius and alpha values to world coordinate frame
     for index in len(r_values):
 
         x,y = r_values[index]*cos(alpha_values[index]),r_values[index]*sin(alpha_values[index])
@@ -159,6 +176,8 @@ def to_world(r_values,alpha_values,endpoints_x,endpoints_y):
 
         r.append(r_world)
         alpha.append(alpha_world)
+    
+    #converting the endpoints to the world coordinate frame
     for index in len(endpoints_x):
 
         x,y = endpoints_x[index],endpoints_y[index]
@@ -179,6 +198,7 @@ def cartesian_to_polar(X,Y):
     r,alpha=radius_negative_conversion(r,alpha)
     return r,alpha
 
+#functin to convert the map values from cartesina to polar coordiantes 
 def map_convert_to_polar(map):
     wall_begin_points=map["wall_begin_points"]
     wall_end_points=map["wall_end_points"]
@@ -193,28 +213,35 @@ def map_convert_to_polar(map):
         r,alpha=cartesian_to_polar(x,y)
         r_values.append(r)
         alpha_values,append(alpha)
+
     return r_values,alpha_values
 
 def pred_update_callback(msg):
+    #function to perform the prediction updat step by using the messge from odom node
     global ekf
     ekf.state =array[msg.x,msg.y,msg.theta]
     ekf.covariance=msg.covariance
 
 
 def feature_update_callback(msg):
+    #storing the different features from the lfeature extract from lidar node
     global features
     features.radius_values = msg.radius_values
     features.alpha_values = msg.alpha_values
     features.endpoints_x = msg.endpoints_x
     features.endpoints_y = msg.endpoints_y
 
+#objects of the required clases
 ekf=ExtendedKalmanFilter()  
 features= Features()
 
 def main():
+
+    #acessing the global class variables
     global ekf
     global features
-    #creating a publisher topic named pose and a node named pose_update 
+
+    #creating a publisher topic named pose_ekf and a node named ekf_node
     ekf_pub = rospy.Publisher("pose_ekf", odometry_custom, queue_size=1)
     map=[]
 
@@ -224,38 +251,41 @@ def main():
     #initialising the rate variable to an appropriate rate 
     rate = rospy.Rate(10) 
   
-    #hardware parameters 
+    #intialising some parameters realted to the ekf 
     ekf.measurement_distance_stddev = rospy.get_param("measurement_distance_stddev")
     ekf.measurement_angle_stddev = rospy.get_param("measurement_angle_stddev")
     ekf.threshold=1.1
 
+    #getting the map begin and end points
     map=dict()
     map["wall_begin_points"] =rospy.get_param("wall_begin_points")
     map["wall_end_points"] = rospy.get_param("wall_end_points")
 
+    #function to convert the map in cartesian to polar coordiantes
     r_ref_values,alpha_ref_values = map_convert_to_polar(map)
 
 
-    #begining pose estimation
-    print("Starting Pose estimation")
-
-    pose= (0, 0 ,0)
     states=[]
     covariances=[]
     current_time = rospy.Time.now()
     last_time = rospy.Time.now()
+
     while not rospy.is_shutdown():
 
         current_time = rospy.Time.now()
+
+        #subscribing the required topics
         rospy.Subscriber('/odom', odometry_custom, pred_update_callback)
         rospy.Subscriber('feature_from_lidar', features, feature_update_callback)
 
 
+        #performin gthe correction part of ekf
         ekf.correction(map,r_ref_values,alpha_ref_values,features.radius_values,features.alpha_values,features.endpoints_x ,features.endpoints_y)
         states.append(ekf.state)
         covariances.append(ekf.covariance)
 
 
+        #preparing the message for publishing and publishing the message
         msg = odometry_custom()
         msg.x = ekf.state[0]
         msg.y = ekf.state[1]
